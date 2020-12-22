44b72397c7aff4f3a65a0464f4c4adb8ca9cf685
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2020-12-22 18:14:35 +0100

    Add app dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -4,17 +4,25 @@ import dependencies.Library
 plugins {
 	id("com.android.application")
 	kotlin("android")
+	kotlin("android.extensions")
 	id("koin")
 }
 
 dependencies {
 	implementation(project(":shared"))
+	implementation(project(":castawayplayer"))
 
+	implementation(Library.viewmodelKtx)
+	implementation(Library.activityKtx)
+	implementation(Library.fragmentKtx)
 	implementation(Library.material)
 	implementation(Library.appcompat)
 	implementation(Library.constraintlayout)
+	implementation(Library.media)
 	implementation(Library.koin)
 	implementation(Library.koinExt)
+	implementation(Library.koinAndroid)
+	implementation(Library.koinViewmodel)
 }
 
 android {
@@ -26,9 +34,19 @@ android {
 		versionCode = App.versionCode
 		versionName = App.versionName
 	}
+
 	buildTypes {
 		getByName("release") {
 			isMinifyEnabled = false
 		}
+	}
+
+	compileOptions {
+		sourceCompatibility(JavaVersion.VERSION_1_8)
+		targetCompatibility(JavaVersion.VERSION_1_8)
+	}
+
+	kotlinOptions {
+		jvmTarget = "1.8"
 	}
 }
