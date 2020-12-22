e6a578f53dccb8d899f2426f1e81196865a7f775
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2020-12-22 18:13:57 +0100

    Rename classpath version vars

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 buildscript {
-	val kotlin_version by extra("1.4.21")
-	val gradle_version by extra("4.1.1")
-	val koin_version by extra("2.2.2")
+	val kotlinVersion by extra("1.4.21")
+	val gradleVersion by extra("4.1.1")
+	val koinVersion by extra("2.2.2")
 
 	repositories {
 		gradlePluginPortal()
@@ -11,9 +11,9 @@ buildscript {
 	}
 
 	dependencies {
-		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-		classpath("com.android.tools.build:gradle:$gradle_version")
-		classpath("org.koin:koin-gradle-plugin:$koin_version")
+		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+		classpath("com.android.tools.build:gradle:$gradleVersion")
+		classpath("org.koin:koin-gradle-plugin:$koinVersion")
 	}
 }
 
