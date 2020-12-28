2099af2d6c81db5900dd7b2bbd974700670170bc
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2020-12-28 22:02:42 +0100

    Feed to feed data extension

---
+++ androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/parser/FeedParser.kt
@@ -0,0 +1,45 @@
+package io.github.lazyengineer.castaway.androidApp.parser
+
+import io.github.lazyengineer.castaway.shared.entity.Episode
+import io.github.lazyengineer.castaway.shared.entity.FeedData
+import io.github.lazyengineer.castaway.shared.entity.PlaybackPosition
+import io.github.lazyengineer.feedparser.model.feed.AtomFeed
+import io.github.lazyengineer.feedparser.model.feed.Feed
+import io.github.lazyengineer.feedparser.model.feed.RSSFeed
+import java.util.*
+
+object FeedParser {
+
+    fun Feed.toFeedData(url: String): FeedData {
+        return when (this) {
+            is RSSFeed -> {
+                FeedData(url = url, title = this.title, episodes = this.items.map {
+                    Episode(
+                        id = UUID.randomUUID().toString(),
+                        title = it.title,
+                        subTitle = it.iTunes?.subtitle ?: "",
+                        description = it.description,
+                        audioUrl = it.media?.url ?: "",
+                        imageUrl = it.mediaNamespace?.thumbnails?.firstOrNull()?.attributes?.url ?: "",
+                        author = it.author ?: "",
+                        playbackPosition = PlaybackPosition(0, Long.MAX_VALUE),
+                    )
+                })
+            }
+            is AtomFeed -> {
+                FeedData(url = url, title = this.title?.value ?: "", episodes = this.entries.map {
+                    Episode(
+                        id = UUID.randomUUID().toString(),
+                        title = it.title?.value ?: "",
+                        subTitle = "",
+                        description = it.summary?.value ?: "",
+                        audioUrl = it.mediaNamespace?.contents?.firstOrNull()?.attributes?.url ?: "",
+                        imageUrl = it.mediaNamespace?.thumbnails?.firstOrNull()?.attributes?.url ?: "",
+                        author = it.authors.firstOrNull()?.uri ?: "",
+                        playbackPosition = PlaybackPosition(0, Long.MAX_VALUE),
+                    )
+                })
+            }
+        }
+    }
+}
