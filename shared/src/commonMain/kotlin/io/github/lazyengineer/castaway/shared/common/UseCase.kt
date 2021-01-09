fa4cafae0fdfcc38400f61553053190c1bb8efbc
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2021-01-09 20:57:47 +0100

    Introduce flowable use case

--- shared/src/commonMain/kotlin/io/github/lazyengineer/castaway/shared/common/UseCase.kt
+++ shared/src/commonMain/kotlin/io/github/lazyengineer/castaway/shared/common/UseCase.kt
@@ -1,17 +1,51 @@
 package io.github.lazyengineer.castaway.shared.common
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+
 abstract class UseCase<out Type, in Params> where Type : Any {
 
-	abstract suspend fun run(params: Params): Result<Type>
+    abstract suspend fun run(params: Params): Result<Type>
 
-	suspend operator fun invoke(params: Params, onSuccess: (Type) -> Unit, onError: (Exception) -> Unit) {
-		when (val result = run(params)) {
+    suspend operator fun invoke(
+		params: Params,
+		onSuccess: (Type) -> Unit,
+		onError: (Exception) -> Unit
+	) {
+        when (val result = run(params)) {
 			is Result.Success -> {
 				onSuccess(result.data)
 			}
 			is Result.Error -> {
 				onError(result.exception)
 			}
-		}
-	}
+        }
+    }
+}
+
+abstract class FlowableUseCase<out Type, in Params> where Type : Any {
+
+    abstract fun run(params: Params): Flow<Result<Type>>
+
+    operator fun invoke(
+		params: Params,
+		onEach: (Type) -> Unit,
+		onError: (Throwable) -> Unit,
+		onComplete: () -> Unit
+	) = run(params)
+        .onEach {
+            when (val result = it) {
+				is Result.Success -> {
+					onEach(result.data)
+				}
+				is Result.Error -> {
+					onError(result.exception)
+				}
+            }
+        }
+        .catch { onError(it) }
+        .onCompletion { onComplete() }
+
 }
