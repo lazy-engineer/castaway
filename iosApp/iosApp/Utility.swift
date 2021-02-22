ea89aaef22a1773dff7b56aa170aa8760a1a1b2b
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2021-02-22 17:14:46 +0100

    Allow position and duration hours in NowPlayingScreen

--- iosApp/iosApp/Utility.swift
+++ iosApp/iosApp/Utility.swift
@@ -5,7 +5,7 @@ class Utility: NSObject {
     private static var timeHMSFormatter: DateComponentsFormatter = {
         let formatter = DateComponentsFormatter()
         formatter.unitsStyle = .positional
-        formatter.allowedUnits = [.minute, .second]
+        formatter.allowedUnits = [.hour, .minute, .second]
         formatter.zeroFormattingBehavior = [.pad]
         return formatter
     }()
@@ -13,7 +13,7 @@ class Utility: NSObject {
     static func formatSecondsToHMS(_ seconds: Double) -> String {
         guard !seconds.isNaN,
             let text = timeHMSFormatter.string(from: seconds) else {
-                return "00:00"
+                return "00:00:00"
         }
          
         return text
