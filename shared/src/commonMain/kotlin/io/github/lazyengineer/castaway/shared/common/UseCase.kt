cd1e908d45438e71bd9196251b49aa94bb2ac9d0
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2021-03-15 22:54:19 +0100

    Change UseCase to UseCaseWrapper

--- shared/src/commonMain/kotlin/io/github/lazyengineer/castaway/shared/common/UseCase.kt
+++ shared/src/commonMain/kotlin/io/github/lazyengineer/castaway/shared/common/UseCase.kt
@@ -1,59 +1,64 @@
 package io.github.lazyengineer.castaway.shared.common
 
-import co.touchlab.stately.ensureNeverFrozen
+import co.touchlab.stately.freeze
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
-abstract class UseCase<out Type, in Params> where Type : Any {
-
-  init {
-	ensureNeverFrozen()
-  }
-
-  abstract suspend fun run(params: Params): Result<Type>
-
-  suspend operator fun invoke(
-	  params: Params,
-	  onSuccess: (Type) -> Unit,
-	  onError: (Exception) -> Unit
-  ) {
-	when (val result = run(params)) {
-		is Result.Success -> {
-			onSuccess(result.data)
-		}
-		is Result.Error -> {
-			onError(result.exception)
-		}
-	}
-  }
-}
-
-abstract class FlowableUseCase<out Type, in Params> where Type : Any {
+sealed class UseCase<out Type, in Params>(private val suspender: suspend () -> Result<Type>) where Type : Any {
 
   init {
-	ensureNeverFrozen()
+	freeze()
   }
 
-  abstract fun run(params: Params): Flow<Result<Type>>
-
-  operator fun invoke(
-	  params: Params,
-	  onEach: (Type) -> Unit,
-	  onError: (Throwable) -> Unit,
-	  onComplete: () -> Unit
-  ) = run(params)
-	.onEach {
-	  when (val result = it) {
-		  is Result.Success -> {
-			  onEach(result.data)
-		  }
-		  is Result.Error -> {
-			  onError(result.exception)
-		  }
+  fun subscribe(
+	scope: CoroutineScope,
+	onSuccess: (Type) -> Unit,
+	onError: (String) -> Unit
+  ) = scope.launch {
+	when (val result = suspender()) {
+	  is Result.Success -> {
+		onSuccess(result.data.freeze())
+	  }
+	  is Result.Error -> {
+		onError((result.exception.message ?: "subscribe error").freeze())
 	  }
 	}
-	.catch { onError(it) }
-	.onCompletion { onComplete() }
+  }.freeze()
 }
+
+class UseCaseWrapper<out Type : Any, in Params>(suspender: suspend () -> Result<Type>) : UseCase<Type, Params>(suspender)
+
+sealed class FlowableUseCase<out Type, in Params>(private val flow: () -> Flow<Result<Type>>) where Type : Any {
+
+  init {
+	freeze()
+  }
+
+  fun subscribe(
+	scope: CoroutineScope,
+	onEach: (Type) -> Unit,
+	onError: (String) -> Unit,
+	onComplete: () -> Unit
+  ) = flow()
+	.onEach {
+	  when (val result = it.freeze()) {
+		is Result.Success -> {
+		  onEach(result.data)
+		}
+		is Result.Error -> {
+		  onError((result.exception.message ?: "subscribe flow error").freeze())
+		}
+	  }
+	}
+	.catch { onError((it.message ?: "catch error").freeze()) }
+	.onCompletion { onComplete() }
+	.launchIn(scope)
+	.freeze()
+}
+
+class FlowableUseCaseWrapper<out Type : Any, in Params>(flow: () -> Flow<Result<Type>>) : FlowableUseCase<Type, Params>(flow)
