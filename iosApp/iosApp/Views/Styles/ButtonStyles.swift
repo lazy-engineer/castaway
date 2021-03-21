64e68e9651604ae0d47ef3d288b01de9a8f88589
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2021-03-21 23:06:46 +0100

    Add neumorphism button style and colors to swiftui

---
+++ iosApp/iosApp/Views/Styles/ButtonStyles.swift
@@ -0,0 +1,129 @@
+import SwiftUI
+
+struct ColorfulButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .contentShape(Circle())
+            .background(
+                ColorfulBackground(isHighlighted: configuration.isPressed, shape: Circle())
+            )
+            .animation(nil)
+    }
+}
+
+struct ColorfulToggleStyle: ToggleStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        Button(action: {
+            configuration.isOn.toggle()
+        }) {
+            configuration.label
+                .contentShape(Circle())
+        }
+        .background(
+            ColorfulBackground(isHighlighted: configuration.isOn, shape: Circle())
+        )
+    }
+}
+
+struct RectangleButtonStyle: ButtonStyle {
+    
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding(30)
+            .contentShape(RoundedRectangle(cornerRadius: 25.0))
+            .background(
+                RoundedRectangle(cornerRadius: 25.0)
+                    .fill(Color.lightThemeBackground)
+                    .shadow(color: Color.lightThemeDarkShadow, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 10, y: 10)
+                    .shadow(color: Color.lightThemeLightShadow, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: -5, y: -5)
+            )
+    }
+}
+
+struct RoundButtonStyle: ButtonStyle {
+    
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding(30)
+            .contentShape(Circle())
+            .background(
+                Circle()
+                    .fill(Color.lightThemeBackground)
+                    .shadow(color: Color.lightThemeDarkShadow, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 10, y: 10)
+                    .shadow(color: Color.lightThemeLightShadow, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: -5, y: -5)
+            )
+    }
+}
+
+struct SimpleButtonStyle: ButtonStyle {
+    
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding(30)
+            .contentShape(Circle())
+            .background(
+                Group {
+                    if configuration.isPressed {
+                        Circle()
+                            .fill(Color.lightThemeBackground)
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.white, lineWidth: 4)
+                                    .blur(radius: 4)
+                                    .offset(x: 2, y: 2)
+                                    .mask(Circle().fill(LinearGradient(Color.black, Color.clear)))
+                            )
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.lightThemeDarkShadow, lineWidth: 8)
+                                    .blur(radius: 4)
+                                    .offset(x: -2, y: -2)
+                                    .mask(Circle().fill(LinearGradient(Color.clear, Color.black)))
+                            )
+                    } else {
+                        Circle()
+                            .fill(Color.lightThemeBackground)
+                            .shadow(color: Color.lightThemeDarkShadow, radius: 10, x: 10, y: 10)
+                            .shadow(color: Color.lightThemeLightShadow, radius: 10, x: -5, y: -5)
+                    }
+                }
+            )
+    }
+}
+
+struct ColorfulBackground<S: Shape>: View {
+    var isHighlighted: Bool
+    var shape: S
+    
+    var body: some View {
+        ZStack {
+            if isHighlighted {
+                shape
+                    .fill(LinearGradient(Color.blueGradientEnd, Color.blueGradientStart))
+                    .overlay(shape.stroke(LinearGradient(Color.blueGradientStart, Color.blueGradientEnd), lineWidth: 4))
+                    .shadow(color: Color.lightThemeDarkShadow, radius: 10, x: -10, y: -10)
+                    .shadow(color: Color.lightThemeLightShadow, radius: 10, x: 10, y: 10)
+            } else {
+                shape
+                    .fill(Color.lightThemeBackground)
+                    .overlay(shape.stroke(LinearGradient(Color.gray.opacity(0.2), Color.lightThemeBackground), lineWidth: 4))
+                    .overlay(
+                        Circle()
+                            .stroke(Color.white, lineWidth: 4)
+                            .blur(radius: 4)
+                            .offset(x: 2, y: 2)
+                            .mask(Circle().fill(LinearGradient(Color.black, Color.clear)))
+                    )
+                    .overlay(
+                        Circle()
+                            .stroke(Color.lightThemeDarkShadow, lineWidth: 8)
+                            .blur(radius: 4)
+                            .offset(x: -2, y: -2)
+                            .mask(Circle().fill(LinearGradient(Color.clear, Color.black)))
+                    )
+                    .shadow(color: Color.lightThemeDarkShadow, radius: 10, x: 10, y: 10)
+                    .shadow(color: Color.lightThemeLightShadow, radius: 10, x: -10, y: -10)
+            }
+        }
+    }
+}
---
+++ iosApp/iosApp/Views/Styles/Color.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+extension Color {
+    static let lightThemeBackground = Color(red: 225 / 255, green: 225 / 255, blue: 235 / 255)
+    
+    static let lightThemeDarkShadow = Color.black.opacity(0.2)
+    static let lightThemeLightShadow = Color.white.opacity(0.7)
+    
+    static let darkGradientStart = Color(red: 50 / 255, green: 60 / 255, blue: 65 / 255)
+    static let darkGradientEnd = Color(red: 25 / 255, green: 25 / 255, blue: 30 / 255)
+    
+    static let blueGradientStart = Color(red: 96 / 255, green: 137 / 255, blue: 255 / 255)
+    static let blueGradientEnd = Color(red: 48 / 255, green: 68 / 255, blue: 125 / 255)
+}
+
+extension LinearGradient {
+    init(_ colors: Color...) {
+        self.init(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing)
+    }
+}
