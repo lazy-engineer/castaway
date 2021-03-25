0ba02d0fe22272fb7a40394da5e7a8cff4328d31
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2021-03-25 23:13:52 +0100

    VStack podcast header with large image

--- iosApp/iosApp/Views/PodcastHeaderView.swift
+++ iosApp/iosApp/Views/PodcastHeaderView.swift
@@ -8,31 +8,43 @@ struct PodcastHeaderView: View {
     @State var feedTitle: String
     
     var body: some View {
-        HStack {
-            if let imageUrl = feedImage {
-                Image(uiImage: imageUrl)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 48, height: 48)
-                    .cornerRadius(10)
-                    .padding(8)
-            } else {
-                Image(systemName: "mic")
-                    .frame(width: 48, height: 48)
-                    .foregroundColor(Color.white)
-                    .background(Color.black)
-                    .clipShape(Rectangle())
-                    .cornerRadius(10)
-                    .padding(8)
-            }
+        VStack {
             
             Text(feedTitle)
-                .font(.title).bold().foregroundColor(theme.colorPalette.textColor)
+                .font(.title2).bold().foregroundColor(theme.colorPalette.textColor)
                 .minimumScaleFactor(0.7)
                 .lineLimit(1)
-                .padding(.trailing, 8)
-            Spacer()
+                .padding(.bottom, 16)
+                
+            if let imageUrl = feedImage {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(theme.colorPalette.background)
+                        .frame(width: 150, height: 150)
+                        .shadow(color: theme.colorPalette.darkShadow, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 10, y: 10)
+                        .shadow(color: theme.colorPalette.lightShadow, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: -5, y: -5)
+
+                    Image(uiImage: imageUrl)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 145, height: 145)
+                        .clipShape(RoundedRectangle(cornerRadius: 24.0))
+                        .shadow(color: theme.colorPalette.darkShadow, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 10, y: 10)
+                        .shadow(color: theme.colorPalette.lightShadow, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: -5, y: -5)
+                }
+            } else {
+                Image(systemName: "mic")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 120, height: 120)
+                    .foregroundColor(theme.colorPalette.background)
+                    .shadow(color: theme.colorPalette.darkShadow, radius: 5, x: 5, y: 5)
+                    .shadow(color: theme.colorPalette.lightShadow, radius: 5, x: -5, y: -5)
+                    .padding(24)
+            }
         }
+        .padding(.top, 24)
+        .padding(.bottom, 24)
     }
 }
 
