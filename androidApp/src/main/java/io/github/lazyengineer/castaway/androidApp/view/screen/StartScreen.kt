6a4fe8827a86f51757dcb64c2f47e4eaad1f9300
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2021-04-12 17:51:46 +0200

    Remember onBoardingFinished state as saveable to keep state on configuration change

--- androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/view/screen/StartScreen.kt
+++ androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/view/screen/StartScreen.kt
@@ -4,14 +4,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.zIndex
 import io.github.lazyengineer.castaway.androidApp.viewmodel.CastawayViewModel
 
 @Composable
 fun StartScreen(viewModel: CastawayViewModel) {
-  val onBoardingFinished = remember { mutableStateOf(false) }
+
+  val onBoardingFinished = rememberSaveable { mutableStateOf(false) }
 
   Box(modifier = Modifier.fillMaxSize()) {
 	if (onBoardingFinished.value.not()) {
