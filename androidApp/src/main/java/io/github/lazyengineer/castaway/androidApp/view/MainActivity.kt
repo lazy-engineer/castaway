96ef3b7bbf1bbba43e456ed9a5640d94ecb9436c
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2021-04-21 14:51:14 +0200

    Delete kmm greeting test function and layout binding

--- androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/view/MainActivity.kt
+++ androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/view/MainActivity.kt
@@ -3,20 +3,13 @@ package io.github.lazyengineer.castaway.androidApp.view
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import io.github.lazyengineer.castaway.androidApp.databinding.ActivityMainBinding
 import io.github.lazyengineer.castaway.androidApp.view.screen.StartScreen
 import io.github.lazyengineer.castaway.androidApp.view.style.ThemeNeumorphism
 import io.github.lazyengineer.castaway.androidApp.viewmodel.CastawayViewModel
-import io.github.lazyengineer.castaway.shared.Greeting
 import org.koin.androidx.viewmodel.ext.android.viewModel
-
-fun greet(): String {
-  return Greeting().greeting()
-}
 
 class MainActivity : AppCompatActivity() {
 
-  private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
   private val viewModel: CastawayViewModel by viewModel()
 
   override fun onCreate(savedInstanceState: Bundle?) {
