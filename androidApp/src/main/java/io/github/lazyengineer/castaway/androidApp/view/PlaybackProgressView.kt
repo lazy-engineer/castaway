4ee5249c748aecca78ddd7e10498142bfd7a82e1
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2021-05-14 23:30:53 +0200

    Show progress track only if there is progress

--- androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/view/PlaybackProgressView.kt
+++ androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/view/PlaybackProgressView.kt
@@ -1,5 +1,6 @@
 package io.github.lazyengineer.castaway.androidApp.view
 
+import androidx.annotation.FloatRange
 import androidx.compose.foundation.Canvas
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -12,16 +13,16 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun PlaybackProgressView(
-  modifier: Modifier,
-  playbackPosition: Float,
+  modifier: Modifier = Modifier,
+  @FloatRange(from = 0.0, to = 1.0) playbackPosition: Float,
   trackColor: Color = MaterialTheme.colors.primary.copy(.25f),
   progressColor: Color = MaterialTheme.colors.primary,
-  thumbDp: Dp = 16.dp,
+  padding: Dp = 16.dp,
   trackStrokeWidth: Dp = 4.dp,
 ) {
   Canvas(modifier) {
-	val start = Offset(thumbDp.toPx(), center.y)
-	val end = Offset(size.width - thumbDp.toPx(), center.y)
+	val start = Offset(padding.toPx(), center.y)
+	val end = Offset(size.width - padding.toPx(), center.y)
 
 	drawLine(
 	  trackColor,
@@ -31,14 +32,16 @@ fun PlaybackProgressView(
 	  Round
 	)
 
-	val sliderValue = Offset(start.x + (end.x - start.x) * playbackPosition, center.y)
+	if (playbackPosition > 0) {
 
-	drawLine(
-	  progressColor,
-	  start,
-	  sliderValue,
-	  trackStrokeWidth.toPx(),
-	  Round
-	)
+	  val sliderValue = Offset(start.x + (end.x - start.x) * playbackPosition, center.y)
+	  drawLine(
+		progressColor,
+		start,
+		sliderValue,
+		trackStrokeWidth.toPx(),
+		Round
+	  )
+	}
   }
 }
