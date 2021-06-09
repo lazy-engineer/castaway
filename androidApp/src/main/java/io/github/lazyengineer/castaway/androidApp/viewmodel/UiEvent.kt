e3e202973fd3e56a3e5c7bed6a9959a5074c8182
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2021-06-09 18:17:50 +0200

    Extract UiEvent to own class file

---
+++ androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/viewmodel/UiEvent.kt
@@ -0,0 +1,21 @@
+package io.github.lazyengineer.castaway.androidApp.viewmodel
+
+import io.github.lazyengineer.castaway.androidApp.view.nowplaying.NowPlayingEpisode
+
+sealed class UiEvent {
+
+  sealed class EpisodeRowEvent : UiEvent() {
+	data class PlayPause(val itemId: String) : EpisodeRowEvent()
+	data class Click(val item: NowPlayingEpisode) : EpisodeRowEvent()
+  }
+
+  sealed class NowPlayingEvent : UiEvent() {
+	object Rewind : NowPlayingEvent()
+	object FastForward : NowPlayingEvent()
+	object ChangePlaybackSpeed : NowPlayingEvent()
+	data class SeekTo(val positionMillis: Long) : NowPlayingEvent()
+	data class EditingPlaybackPosition(val position: Long) : NowPlayingEvent()
+	data class EditingPlayback(val editing: Boolean) : NowPlayingEvent()
+	data class PlayPause(val itemId: String) : NowPlayingEvent()
+  }
+}
