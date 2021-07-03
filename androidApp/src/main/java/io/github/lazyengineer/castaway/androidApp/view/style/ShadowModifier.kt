46ea9683d8f850531ee2499b9051c2ed592e6cf4
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2021-07-03 22:41:59 +0200

    Use argb color in shadow modifier

--- androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/view/style/ShadowModifier.kt
+++ androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/view/style/ShadowModifier.kt
@@ -2,26 +2,23 @@ package io.github.lazyengineer.castaway.androidApp.view.style
 
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 fun Modifier.shadow(
-  color: Color,
-  alpha: Float = 0.2f,
+  shadowColor: Int,
+  paintColor: Int,
   borderRadius: Dp = 0.dp,
   shadowRadius: Dp = 20.dp,
   offsetY: Dp = 0.dp,
   offsetX: Dp = 0.dp
 ) = this.drawBehind {
-  val transparentColor = android.graphics.Color.toArgb(color.copy(alpha = 0.0f).value.toLong())
-  val shadowColor = android.graphics.Color.toArgb(color.copy(alpha = alpha).value.toLong())
   this.drawIntoCanvas {
 	val paint = Paint()
 	val frameworkPaint = paint.asFrameworkPaint()
-	frameworkPaint.color = transparentColor
+	frameworkPaint.color = paintColor
 	frameworkPaint.setShadowLayer(
 	  shadowRadius.toPx(),
 	  offsetX.toPx(),
