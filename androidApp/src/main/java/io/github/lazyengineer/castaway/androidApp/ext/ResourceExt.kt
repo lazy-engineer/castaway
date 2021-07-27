9f30f3a3c47c234ba8c13460b20acd7fcbb0bf34
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2021-07-27 22:51:39 +0200

    Rename SharedColor to CommonColor an androidApp side

--- androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/ext/ResourceExt.kt
+++ androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/ext/ResourceExt.kt
@@ -1,8 +1,8 @@
 package io.github.lazyengineer.castaway.androidApp.ext
 
 import androidx.compose.ui.graphics.Color
-import io.github.lazyengineer.castaway.shared.resource.Color as SharedColor
+import io.github.lazyengineer.castaway.shared.resource.CommonColor
 
-fun SharedColor.toColor(): Color {
+fun CommonColor.toColor(): Color {
   return Color(red = red, green = green, blue = blue, alpha = (alpha * 255).toInt())
 }
