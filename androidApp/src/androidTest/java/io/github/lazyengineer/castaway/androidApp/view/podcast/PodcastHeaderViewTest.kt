118adb6d8a3b71d76634e4655574a9af196a2180
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2021-08-20 23:48:18 +0200

    Add android PodcastHeaderView tests

---
+++ androidApp/src/androidTest/java/io/github/lazyengineer/castaway/androidApp/view/podcast/PodcastHeaderViewTest.kt
@@ -0,0 +1,44 @@
+package io.github.lazyengineer.castaway.androidApp.view.podcast
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import org.junit.*
+
+class PodcastHeaderViewTest {
+
+  @get:Rule
+  val composeTestRule = createComposeRule()
+
+  @Test
+  fun podcastHeaderView_shouldDisplayPodcastTitle() {
+	val podcastTitle = "Header Test"
+
+	composeTestRule.setContent {
+	  PodcastHeaderView(
+		modifier = Modifier,
+		title = podcastTitle,
+		imageUrl= "test-image.url"
+	  )
+	}
+
+	composeTestRule.onNodeWithText(podcastTitle)
+	  .assertIsDisplayed()
+  }
+
+  @Test
+  fun podcastHeaderView_shouldDisplayPodcastImage() {
+	composeTestRule.setContent {
+	  PodcastHeaderView(
+		modifier = Modifier,
+		title = "Header Test",
+		imageUrl= "test-image.url"
+	  )
+	}
+
+	composeTestRule.onNodeWithContentDescription("Podcast header image")
+	  .assertIsDisplayed()
+  }
+}
