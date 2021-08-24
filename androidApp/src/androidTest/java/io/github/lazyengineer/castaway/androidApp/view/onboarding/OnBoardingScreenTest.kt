aa626ded5ab7c66128fefda6ee1c9e2ecf9c30ca
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2021-08-25 00:03:08 +0200

    Assert onboarding choose theme description show proper text on dark and light mode

--- androidApp/src/androidTest/java/io/github/lazyengineer/castaway/androidApp/view/onboarding/OnBoardingScreenTest.kt
+++ androidApp/src/androidTest/java/io/github/lazyengineer/castaway/androidApp/view/onboarding/OnBoardingScreenTest.kt
@@ -2,6 +2,7 @@ package io.github.lazyengineer.castaway.androidApp.view.onboarding
 
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import org.junit.*
@@ -37,5 +38,41 @@ class OnBoardingScreenTest {
 
 	composeTestRule.onNodeWithContentDescription("Choose theme description")
 	  .assertIsDisplayed()
+  }
+
+  @Test
+  fun onBoardingScreen_shouldEqualTextIfDarkMode() {
+	composeTestRule.setContent {
+	  OnBoardingScreen(
+		modifier = Modifier,
+		darkTheme = true,
+		switchTheme = {},
+		finished = {}
+	  )
+	}
+
+	composeTestRule.onNodeWithContentDescription("Choose theme description")
+	  .assertIsDisplayed()
+	  .assertTextEquals("The night is darkest just before the dawn. And I promise you, the dawn is coming.\n - Harvey Dent")
+  }
+
+  @Test
+  fun onBoardingScreen_shouldEqualTextIfLightMode() {
+	composeTestRule.setContent {
+	  OnBoardingScreen(
+		modifier = Modifier,
+		darkTheme = false,
+		switchTheme = {},
+		finished = {}
+	  )
+	}
+
+	composeTestRule.onNodeWithContentDescription("Choose theme description")
+	  .assertTextEquals(
+		"- Hamid: What's that? \n" +
+				" - Rambo: It's blue light. \n" +
+				" - Hamid: What does it do? \n" +
+				" - Rambo: It turns blue."
+	  )
   }
 }
