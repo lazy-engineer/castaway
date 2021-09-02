fd008544c0c5184282bb1f0e84ea29fff0c12d31
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2021-09-02 23:47:47 +0200

    Use system theme mode as default mode

--- androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/view/activity/MainActivity.kt
+++ androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/view/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package io.github.lazyengineer.castaway.androidApp.view.activity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import io.github.lazyengineer.castaway.androidApp.theme.CastawayTheme
@@ -18,7 +19,8 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
 	super.onCreate(savedInstanceState)
 	setContent {
-	  val darkMode = rememberSaveable { mutableStateOf(true) }
+	  val systemThemeMode = isSystemInDarkTheme()
+	  val darkMode = rememberSaveable { mutableStateOf(systemThemeMode) }
 	  val theme = rememberSaveable { mutableStateOf(MATERIAL) }
 
 	  CastawayTheme(theme.value, darkMode.value) {
