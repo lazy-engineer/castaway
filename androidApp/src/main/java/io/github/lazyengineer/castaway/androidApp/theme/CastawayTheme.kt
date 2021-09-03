96d9c262e848b295f87e41b7695ffdefe69a1afd
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2021-09-03 12:13:06 +0200

    Start to implement castaway theme provider

--- androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/theme/CastawayTheme.kt
+++ androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/theme/CastawayTheme.kt
@@ -2,12 +2,29 @@ package io.github.lazyengineer.castaway.androidApp.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.staticCompositionLocalOf
 import io.github.lazyengineer.castaway.androidApp.theme.ThemeType.MATERIAL
 import io.github.lazyengineer.castaway.androidApp.theme.ThemeType.NEUMORPHISM
+import io.github.lazyengineer.castaway.shared.resource.Colors
 
 @Composable
-fun CastawayTheme(themeType: ThemeType, darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) =
+fun CastawayTheme(
+  themeType: ThemeType,
+  darkTheme: Boolean = isSystemInDarkTheme(),
+  content: @Composable() () -> Unit) =
+
   when (themeType) {
 	MATERIAL -> ThemeMaterial(darkTheme, content)
 	NEUMORPHISM -> ThemeNeumorphism(darkTheme, content)
   }
+
+object CastawayTheme {
+
+  val colors: Colors
+  	@Composable
+  	get() = LocalCastawayColors.current
+}
+
+private val LocalCastawayColors = staticCompositionLocalOf<Colors> {
+  error("No JetsnackColorPalette provided")
+}
