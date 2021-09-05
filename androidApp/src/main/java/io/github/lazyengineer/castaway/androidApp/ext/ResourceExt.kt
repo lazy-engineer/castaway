9af6c79b9318dff56a25cb0f5e8b3394ee4b5fc4
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2021-09-05 22:32:59 +0200

    Add ThemedColor toColor extension

--- androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/ext/ResourceExt.kt
+++ androidApp/src/main/java/io/github/lazyengineer/castaway/androidApp/ext/ResourceExt.kt
@@ -2,7 +2,12 @@ package io.github.lazyengineer.castaway.androidApp.ext
 
 import androidx.compose.ui.graphics.Color
 import io.github.lazyengineer.castaway.shared.resource.CommonColor
+import io.github.lazyengineer.castaway.shared.resource.ThemedColor
 
 fun CommonColor.toColor(): Color {
   return Color(red = red, green = green, blue = blue, alpha = (alpha * 255).toInt())
+}
+
+fun ThemedColor.toColor(): Color {
+  return Color(red = this.color.red, green = this.color.green, blue = this.color.blue, alpha = (this.color.alpha * 255).toInt())
 }
